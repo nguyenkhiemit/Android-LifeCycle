4263d4ea386bb215b25baef18dd3520858a04c14
Khiem Nguyen <nguyenkhiemit@gmail.com>
2018-10-25 11:19:31 +0700

    log fragment life cycle

--- LifeCycleSample/app/src/main/java/com/newgate/lifecycle/FirstFragment.kt
+++ LifeCycleSample/app/src/main/java/com/newgate/lifecycle/FirstFragment.kt
@@ -2,6 +2,7 @@ package com.newgate.lifecycle
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,50 +13,61 @@ class FirstFragment: BaseFragment() {
 
     override fun onAttach(context: Context?) {
         super.onAttach(context)
+        Log.e("LifeCycle FirstFragment:", "onAttach")
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.e("LifeCycle FirstFragment:", "onCreate")
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater!!.inflate(R.layout.fragment_first, container, false)
+        Log.e("LifeCycle FirstFragment:", "onCreateView")
         view.nextFragButton.setOnClickListener {
-            navigation.openFragment(R.id.containerFrame, SecondFragment(), NavigationManager.Type.ADD, null)
+            navigation.openFragment(R.id.containerFrame, SecondFragment(), NavigationManager.Type.REPLACE, null)
         }
         return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        Log.e("LifeCycle FirstFragment:", "onActivityCreated")
     }
 
     override fun onStart() {
         super.onStart()
+        Log.e("LifeCycle FirstFragment:", "onStart")
     }
 
     override fun onResume() {
         super.onResume()
+        Log.e("LifeCycle FirstFragment:", "onResume")
     }
 
     override fun onPause() {
         super.onPause()
+        Log.e("LifeCycle FirstFragment:", "onPause")
     }
 
     override fun onStop() {
         super.onStop()
+        Log.e("LifeCycle FirstFragment:", "onStop")
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        Log.e("LifeCycle FirstFragment:", "onDestroyView")
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        Log.e("LifeCycle FirstFragment:", "onDestroy")
     }
 
     override fun onDetach() {
         super.onDetach()
+        Log.e("LifeCycle FirstFragment:", "onDetach")
     }
 
 }
--- LifeCycleSample/app/src/main/java/com/newgate/lifecycle/SecondFragment.kt
+++ LifeCycleSample/app/src/main/java/com/newgate/lifecycle/SecondFragment.kt
@@ -1,6 +1,8 @@
 package com.newgate.lifecycle
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +10,16 @@ import com.newgate.basekotlinmvvm.base.view.BaseFragment
 import kotlinx.android.synthetic.main.fragment_second.view.*
 
 class SecondFragment: BaseFragment() {
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        Log.e("LifeCycle SecondFragment:", "onAttach")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.e("LifeCycle SecondFragment:", "onCreate")
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view = inflater!!.inflate(R.layout.fragment_second, container, false)
@@ -17,4 +29,43 @@ class SecondFragment: BaseFragment() {
         return view
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        Log.e("LifeCycle SecondFragment:", "onActivityCreated")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e("LifeCycle SecondFragment:", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("LifeCycle SecondFragment:", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("LifeCycle SecondFragment:", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e("LifeCycle SecondFragment:", "onStop")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.e("LifeCycle SecondFragment:", "onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("LifeCycle SecondFragment:", "onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.e("LifeCycle SecondFragment:", "onDetach")
+    }
 }
